a9c551865e3779bf0eaa352b2659e5393b5eea19
vans163 <vans163>
2019-11-11 12:23:50 -0500

    format ex

--- lib/http.ex
+++ lib/http.ex
@@ -1,33 +1,43 @@
 defmodule Rush.Http do
-	def headers_json(headers) do
-		Map.merge(headers, %{"Content-Type"=> "application/json"})
-	end
-	def headers_cors(headers) do
-		Map.merge(headers, 
-            %{"Access-Control-Allow-Origin"=> "*",
-              "Access-Control-Allow-Methods"=> "GET, POST, HEAD, PUT, DELETE",
-              "Access-Control-Allow-Headers"=> "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With"
-        })
-	end
-        
-    def http(:'GET', "/rushing.json", _q, h, _, s) do
-        rushing = File.read!("./priv/rushing.json")
-        json_str = JSX.encode!(rushing)
+  def headers_json(headers) do
+    Map.merge(headers, %{"Content-Type" => "application/json"})
+  end
 
-        {code, headersReply, binReply, s}
-            = :stargate_plugin.serve_static_bin(json_str, h, s)
-        headersReply = headersReply
-        	|> headers_json()
-        	|> headers_cors()
-        {code, headersReply, binReply, s}
-    end
+  def headers_cors(headers) do
+    Map.merge(
+      headers,
+      %{
+        "Access-Control-Allow-Origin" => "*",
+        "Access-Control-Allow-Methods" => "GET, POST, HEAD, PUT, DELETE",
+        "Access-Control-Allow-Headers" =>
+          "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With"
+      }
+    )
+  end
 
-    def http(:'GET', path, _q, h, _, s) do
-        path = cond do
-            path == "/" -> "index.html"
-            path == "" -> "index.html"
-            true -> path
-        end
-        :stargate_static_file.serve_static("./priv/panel/build/", path, h, s)
-    end
+  def http(:GET, "/rushing.json", _q, h, _, s) do
+    rushing = File.read!("./priv/rushing.json")
+    json_str = JSX.encode!(rushing)
+
+    {code, headersReply, binReply, s} =
+      :stargate_plugin.serve_static_bin(json_str, h, s)
+
+    headersReply =
+      headersReply
+      |> headers_json()
+      |> headers_cors()
+
+    {code, headersReply, binReply, s}
+  end
+
+  def http(:GET, path, _q, h, _, s) do
+    path =
+      cond do
+        path == "/" -> "index.html"
+        path == "" -> "index.html"
+        true -> path
+      end
+
+    :stargate_static_file.serve_static("./priv/panel/build/", path, h, s)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -1,22 +1,25 @@
 defmodule Rush.Mixfile do
-    use Mix.Project
+  use Mix.Project
 
-    def project, do: [
-        app: :rush,
-        version: "0.0.1",
-        elixir: "~> 1.9",
-        build_embedded: Mix.env == :prod,
-        start_permanent: Mix.env == :prod,
-        deps: deps(),
+  def project,
+    do: [
+      app: :rush,
+      version: "0.0.1",
+      elixir: "~> 1.9",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
     ]
 
-    def application, do: [
-        applications: [:logger],
-        mod: {Rush, []}
+  def application,
+    do: [
+      applications: [:logger],
+      mod: {Rush, []}
     ]
 
-    def deps, do: [
-        {:exjsx, "~> 4.0.0"},
-        {:stargate, git: "https://github.com/vans163/stargate.git"},
+  def deps,
+    do: [
+      {:exjsx, "~> 4.0.0"},
+      {:stargate, git: "https://github.com/vans163/stargate.git"}
     ]
 end
